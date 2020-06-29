3c359562cd55a7922964840af31980673f59099e
Jack Hayter <jack.hayter@jane.app>
2020-06-29 14:35:35 -0700

    Bye bye webpack

--- Gemfile
+++ Gemfile
@@ -6,17 +6,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 gem 'bcrypt', '~> 3.1.7'
 gem 'date_validator'
 gem 'pg', '>= 0.18', '< 2.0'
-gem 'puma', '~> 4.1'
+gem 'puma'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.2'
-gem 'redis', '~> 4.0'
-gem 'reform'
-gem 'reform-rails'
-gem 'sass-rails', '>= 6'
 gem 'simple_form'
-gem 'turbolinks', '~> 5'
 gem 'valid_email2'
-gem 'view_component'
-gem 'webpacker', '~> 4.0'
 
 group :development do
   gem 'byebug'
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,21 +32,11 @@ Rails.application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
-  # Store uploaded files on the local file system in a temporary directory.
-  # config.active_storage.service = :test
-
-  # config.action_mailer.perform_caching = false
-
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  # config.action_mailer.delivery_method = :test
-
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations.
-  # config.action_view.raise_on_missing_translations = true
+  config.action_view.raise_on_missing_translations = true
 
   config.credentials.content_path = Rails.root.join('config', 'credentials', 'test.yml.enc')
   config.credentials.key_path = Rails.root.join('config', 'credentials', 'test.key')
